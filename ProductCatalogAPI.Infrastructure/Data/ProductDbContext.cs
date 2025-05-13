7e2ba40075c17277e54bf7b079b542c920dc7dac
Ronito Asinjo Jr <ronitoasinjo@yahoo.com>
2025-05-13 23:49:53 +0800

    code first initial migration

--- ProductCatalogAPI.Infrastructure/Data/ProductDbContext.cs
+++ ProductCatalogAPI.Infrastructure/Data/ProductDbContext.cs
@@ -9,5 +9,17 @@ namespace ProductCatalogAPI.Infrastructure.Data
         { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                // Set precision and scale for decimal type 'Price' column
+                entity.Property(e => e.Price)
+                      .HasColumnType("decimal(18,2)"); // Example: 18 digits, 2 decimal places
+            });
+        }
     }
 }
